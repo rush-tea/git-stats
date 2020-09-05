e35d64ed895dddfd8a8baeec24207735c8965a8c
Manish Singh <astromanishsingh@gmail.com>
2020-09-05 16:26:23 +0530

    SearchPage styled

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import SearchPageHeader from './SearchPageHeader'
+import './css/SearchPage.css'
 
 function Profile() {
     const [name, setUsername] = useState({
@@ -10,10 +12,15 @@ function Profile() {
         setUsername({userName: e.target.value})
     } 
     return (
-        <div>
-            <input type="text" value={name.userName} onChange={changeUsername} />
-            <button> <Link to={'/' + name.userName}>Find </Link> </button>
-        </div>
+            <div className="cntnr">
+                <SearchPageHeader />
+                <form>
+                    <div className="form-group">
+                        <input type="text" value={name.userName} onChange={changeUsername} placeholder="Enter Username" className="form-control"/>
+                    </div>
+                    <button className="btn btn-dark"> <Link to={'/' + name.userName} className="text-light">Search </Link> </button>
+                </form>
+            </div>
     )
 }
 
---
+++ src/components/SearchPageHeader.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchPageHeader = () => {
+    return(
+        <h1 className="main-heading display-4"><img height="52" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/github.svg" />itHub Stats</h1>
+    );
+}
+
+export default SearchPageHeader
