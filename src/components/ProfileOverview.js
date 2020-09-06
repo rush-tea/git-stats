cb9b43514ce43be410179b666c524b65e5e4c580
rush-tea <iec2019024@iiita.ac.in>
2020-09-07 04:49:47 +0530

    bugs fixed

--- src/components/ProfileOverview.js
+++ src/components/ProfileOverview.js
@@ -1,21 +1,25 @@
 import React, { useEffect } from 'react';
 
 function ProfileOverview(props) {
-    useEffect(() => {
-        console.log(props);
-    },[])
     return (
         <>
             <div className="logo">
-                <img src={props.profile.avatar_url} alt="logo" />
+            {
+                    props.profile.avatar_url && <img src={props.profile.avatar_url} alt="logo" />
+            }   
                 <div className="p-name">
-                    <div>{props.profile.name}</div>
-                    <div> {props.profile.login} </div>
+                {
+                        props.profile.name && <div>{props.profile.name}</div>
+                }
+                {
+                        props.profile.login && <div> { props.profile.login } </div>
+                }   
+                    
                     {
-                        props.profile.bio.length > 0 && <div className="p-bio"><i className="fa fa-crosshairs" aria-hidden="true"></i> {props.profile.bio}</div>
+                        props.profile.bio && <div className="p-bio"><i className="fa fa-crosshairs" aria-hidden="true"></i> {props.profile.bio}</div>
                     }
                     {
-                        props.profile.company.length > 0 && <div className="p-company"><i className="fa fa-renren" aria-hidden="true"></i> {props.profile.company}</div>
+                        props.profile.company && <div className="p-company"><i className="fa fa-renren" aria-hidden="true"></i> {props.profile.company}</div>
                     }
                 </div>
                 
