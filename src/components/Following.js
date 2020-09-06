bf5736e1e997cc18a342221e44785100aab33b5a
Manish Singh <astromanishsingh@gmail.com>
2020-09-07 04:07:02 +0530

    in following section on clicking username one is directed to git-stat of that user

--- src/components/Following.js
+++ src/components/Following.js
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Following(props) {
 
@@ -9,41 +9,41 @@ function Following(props) {
     const [pageNo, setPageNo] = useState(1);
 
     useEffect(() => {
-        if(pageNo < 1) {
+        if (pageNo < 1) {
             setPageNo(1);
         }
         axios({
             method: 'get',
-            url: 'https://api.github.com/users/' + props.userName + '/following?page=' + pageNo +  '&per_page=30',
-        },{
+            url: 'https://api.github.com/users/' + props.userName + '/following?page=' + pageNo + '&per_page=30',
+        }, {
             headers: {
                 authorization: `token ${process.env.REACT_APP_API_KEY}`
             }
         }).then(function (response) {
             setFollowings(response.data);
         });
-    },[pageNo]);
+    }, [pageNo]);
 
     return (
         <>
-        <div className="act-heading">Following</div>
-        <div className="f-div">
-        {
-            followings.map(res => {
-                return(
-                    <div key={res.id} className="f-details">
-                        <img src={res.avatar_url} alt="logo" className="f-logo" />
-                        <span className="f-username">{res.login} </span>
-                        <span className='f-git-link'><Link to={res.html_url}>Github <i className="fa fa-github-alt" aria-hidden="true"></i></Link></span>
-                    </div>
-                )
-            })
-        }
-        </div>
-        <div className='page-button'>
-            <button onClick={() => setPageNo(pageNo - 1)}>Prev</button>
-            <button onClick={() => setPageNo(pageNo + 1)}>Next</button>
-        </div>
+            <div className="act-heading">Following</div>
+            <div className="f-div">
+                {
+                    followings.map(res => {
+                        return (
+                            <div key={res.id} className="f-details">
+                                <img src={res.avatar_url} alt="logo" className="f-logo" />
+                                <span className="f-username"><button onClick={() => window.location.reload()}><Link to={`/${res.login}`} className="cool-link">{res.login}</Link></button></span>
+                                <span className='f-git-link'><Link to={res.html_url}>Github <i className="fa fa-github-alt" aria-hidden="true"></i></Link></span>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <div className='page-button'>
+                <button onClick={() => setPageNo(pageNo - 1)}>Prev</button>
+                <button onClick={() => setPageNo(pageNo + 1)}>Next</button>
+            </div>
         </>
     )
 }
